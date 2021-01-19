5c0e51227750b66ab04c055819cd4f441f7760df
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2021-01-19 21:54:37 +0800

    Add files via upload

---
+++ MCDream_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef MCDREAM_PRIVATE_H
+#define MCDREAM_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.172"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	172
+#define COMPANY_NAME	"ExL Studios"
+#define FILE_VERSION	"1.0.0.172"
+#define FILE_DESCRIPTION	"MCDream"
+#define INTERNAL_NAME	"MCDream"
+#define LEGAL_COPYRIGHT	"Copyrights (C) 2021 ExL Studios"
+#define LEGAL_TRADEMARKS	"ExecuteIf"
+#define ORIGINAL_FILENAME	"MCDream.exe"
+#define PRODUCT_NAME	"MCDream.exe"
+#define PRODUCT_VERSION	"1.0.0.172"
+
+#endif /*MCDREAM_PRIVATE_H*/
